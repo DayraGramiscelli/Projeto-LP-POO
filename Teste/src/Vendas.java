5261651e0626914c5a84a4ae3b33ce69f78a246b
DayraGramiscelli <131476671+DayraGramiscelli@users.noreply.github.com>
2024-05-23 20:28:40 -0300

    Add files via upload

---
+++ Teste/src/Vendas.java
@@ -0,0 +1,71 @@
+public class Vendas {
+
+
+    private String IdCliente;
+    private String Vendedor;
+    private String Armas;
+    private String Municao;
+    private String Npedido;
+    private String Data;
+    private String Equipamento;
+
+    public String getIdCliente() {
+        return IdCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        IdCliente = idCliente;
+    }
+
+    public String getVendedor() {
+        return Vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        Vendedor = vendedor;
+    }
+
+    public String getArmas() {
+        return Armas;
+    }
+
+    public void setArmas(String armas) {
+        Armas = armas;
+    }
+
+    public String getMunicao() {
+        return Municao;
+    }
+
+    public void setMunicao(String municao) {
+        Municao = municao;
+    }
+
+    public String getNpedido() {
+        return Npedido;
+    }
+
+    public void setNpedido(String npedido) {
+        Npedido = npedido;
+    }
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String data) {
+        Data = data;
+    }
+
+    public String getEquipamento() {
+        return Equipamento;
+    }
+
+    public void setEquipamento(String equipamento) {
+        Equipamento = equipamento;
+    }
+
+
+
+
+}
